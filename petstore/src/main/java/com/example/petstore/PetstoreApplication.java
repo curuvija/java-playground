b34d3eb899050c496e4eed226e306a3f35d70509
Milos Curuvija <milos.curuvija@crealogix.com>
2022-05-16 15:26:40 +0200

    add hello world example

--- petstore/src/main/java/com/example/petstore/PetstoreApplication.java
+++ petstore/src/main/java/com/example/petstore/PetstoreApplication.java
@@ -2,12 +2,19 @@ package com.example.petstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PetstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetstoreApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
 }
