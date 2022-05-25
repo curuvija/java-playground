3fe2042900bb82bfaa79cecd73ce76468f223dd6
Milos Curuvija <milos.curuvija@crealogix.com>
2022-05-25 15:41:58 +0200

    add liquibase support

---
+++ petstore/src/main/java/com/example/petstore/model/Location.java
@@ -0,0 +1,41 @@
+package com.example.petstore.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// TODO: Finish this one https://zetcode.com/springboot/postgresql/
+
+@Entity
+@Table(name="locations")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    
+    public Location() {
+
+    }
+
+    public Location(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
